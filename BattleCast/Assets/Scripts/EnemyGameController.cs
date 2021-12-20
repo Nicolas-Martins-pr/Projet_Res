a4645d81463cd04764f96878d76c475fcb1fe422
M5Altaïr <guillaume.vinet.38@gmail.com>
2021-12-20 00:04:39 -0500

    Add enemyhealth bar

--- BattleCast/Assets/Scripts/EnemyGameController.cs
+++ BattleCast/Assets/Scripts/EnemyGameController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnemyGameController : MonoBehaviour
+public class EnemyGameController : NetworkBehaviour
 {
     [SerializeField]
     private float walkSpeed;
@@ -12,27 +14,101 @@ public class EnemyGameController : MonoBehaviour
     private bool movingRight;
 
     [SerializeField]
+    private int enemyMaxHP;
+
+    private NetworkVariable<int> currentEnemyHP;
+
+    [SerializeField]
     private Transform groundCheckPos;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private Slider healthBar;
+
+
+    private void Start()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * walkSpeed);
-
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheckPos.position, Vector2.down, 2f);
-
-        if (groundInfo.collider == false)
+        if (IsServer)
         {
-            if (movingRight)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
-            } else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
-        }
+            currentEnemyHP.Value = enemyMaxHP;
+            healthBar.minValue = 0;
+            healthBar.maxValue = currentEnemyHP.Value;
+            healthBar.value = currentEnemyHP.Value;
+            UpdateEnemyHPClientRPC(currentEnemyHP.Value);
+        }       
     }
 
+    // Update is called once per frame // Server Side
+    void Update()
+    {
+        //TO remove
+        if (Input.GetKeyDown(KeyCode.D))
+            DamageEnemy(10);
+
+        if (IsServer)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * walkSpeed);
+
+            RaycastHit2D groundInfo = Physics2D.Raycast(groundCheckPos.position, Vector2.down, 2f);
+
+            if (groundInfo.collider == false)
+            {
+                if (movingRight)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+
+                    var rotationVector = healthBar.gameObject.transform.rotation.eulerAngles;
+                    rotationVector.z = 180;
+                    UpdateEnemyHPBarRotationClientRPC(rotationVector);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+
+                    var rotationVector = healthBar.gameObject.transform.rotation.eulerAngles;
+                    rotationVector.z = 0;
+                    UpdateEnemyHPBarRotationClientRPC(rotationVector);
+                    movingRight = true;
+                }
+            }
+        }
+
+
+        //handles the slider bug
+        if (currentEnemyHP.Value <= 7)
+        {
+            EndGame();
+        }
+    }
+    public void DamageEnemy(int damageTaken)
+    {
+
+        if (IsServer)
+        {
+            currentEnemyHP.Value -= damageTaken;
+            healthBar.value = currentEnemyHP.Value;
+            UpdateEnemyHPClientRPC(currentEnemyHP.Value);
+        }
+
+    }
+
+
+    [ClientRpc]
+    public void UpdateEnemyHPBarRotationClientRPC(Vector3 newRotation)
+    {
+        healthBar.gameObject.transform.rotation = Quaternion.Euler(newRotation);
+    }
+
+
+    [ClientRpc]
+    public void UpdateEnemyHPClientRPC(int newHP)
+    {
+        healthBar.value = newHP;
+    }
+
+    private void EndGame()
+    {
+        //TODO
+        //endgame here
+    }
 }
